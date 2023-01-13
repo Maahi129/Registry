74631007745c16c7333905cce70b21c676c28c32
Maahi129 <ponnurumahesh129@gmail.com>
2023-01-13 11:06:17 +0530

    updatedversion

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
 
+
 export default configureStore({
     reducer:{
         user: userReducer,
     },
 })
+
+
